5a4601486dfe266108d5a5b5ee2af91183f07703
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-14 21:02:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,6 +128,7 @@ end
 
 # Write code here
 def num_points_scored(name)
-  puts game_hash[:home][:players][:player_name]
+  home = game_hash[:home][:players]
+  away = game_hash[:away][:players]
 
 end
